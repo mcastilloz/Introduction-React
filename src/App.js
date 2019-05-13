0a9c3be1f4a1530e14127e674151b164fd5169e8
manny <manuel.castillo@freedomdrivenllc.com>
2019-05-13 08:13:23 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// data
+import { todos } from './todos.json';
+
+// subcomponents
+import TodoForm from './components/TodoForm';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos
+    }
+    this.handleAddTodo3 = this.handleAddTodo3.bind(this);
+  }
+
+  removeTodo(index) {
+      this.state.todos.splice(index, 1); 
+      this.setState({todos: this.state.todos });
+
+ 
+  }
+
+  updateRec(index){
+    this.state.todos[index].title ="MANNY";
+    this.setState({todos: this.state.todos });
+
+  }
+
+  handleAddTodo3(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
+  render() {
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-md-4" key={i}>
+          <div className="card mt-4">
+            <div className="card-title text-center">
+              <h3>{todo.title}</h3>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
+            </div>
+            <div className="card-body">
+              {todo.description}
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)}>
+                Delete
+              </button>
+              <button
+                className="btn "
+                onClick={this.updateRec.bind(this, i)}>
+                UPDATE
+              </button>
+            </div>
+          </div>
+        </div>
+      )
+    });
+
+    // RETURN THE COMPONENT
+    return (
+      <div className="App">
+
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="/">
+            Tasks
+            <span className="badge badge-pill badge-light ml-2">
+              {this.state.todos.length}
+            </span>
+          </a>
+        </nav>
+
+        <div className="container">
+          <div className="row mt-4">
+
+            <div className="col-md-4 text-center">
+                <img src={logo} className="App-logo" alt="logo" />
+              <TodoForm onAddTodo={this.handleAddTodo3}></TodoForm>
+            </div>
+
+            <div className="col-md-8">
+              <div className="row">
+                {todos}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
